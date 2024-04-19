0a1505faef86c7574cee1f2bca3d46800429a7f5
howhi <howhiisahop@gmail.com>
2024-04-19 16:41:44 -0400

    Updated Volunteer Hours

--- Student.java
+++ Student.java
@@ -1,13 +1,21 @@
 public class Student
-  {
-    public String firstName;
-    public String lastName;
-    public int volunteerHours;
+{
+	public String firstName;
+    	public String lastName;
+    	public int volunteerHours;
+    
 	
-    public Student(String firstName, String lastName, int volunteerHours)
-    {
-    	this.firstName = firstName;
-	this.lastName = lastName;
-	this.volunteerHours = volunteerHours;
-    }
-  }
+    	public Student(String firstName, String lastName, int volunteerHours)
+    	{
+    		this.firstName = firstName;
+		this.lastName = lastName;
+		this.volunteerHours = volunteerHours;
+    	}
+
+	public String toString()
+	{
+		return firstName + " " + lastName + " has " + volunteerHours + " volunteer hours.";
+	}
+
+
+}
