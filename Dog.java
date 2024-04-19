c025318f6291e75c67735878ceb2ebb84bccbce3
howhi <howhiisahop@gmail.com>
2024-04-19 14:51:30 -0400

    Updated Dog and Student

---
+++ Dog.java
@@ -0,0 +1,11 @@
+public class Dog
+  {
+	private int age;
+	private String noise;
+
+	public Dog(int age, String noise)
+	{
+		this.age = age;
+		this.noise = noise;
+	} 
+  }
--- Student.java
+++ Student.java
@@ -2,10 +2,12 @@ public class Student
   {
     public String firstName;
     public String lastName;
+    public int volunteerHours;
 	
-    public Student(String firstName, String lastName)
+    public Student(String firstName, String lastName, int volunteerHours)
     {
     	this.firstName = firstName;
 	this.lastName = lastName;
+	this.volunteerHours = volunteerHours;
     }
   }
